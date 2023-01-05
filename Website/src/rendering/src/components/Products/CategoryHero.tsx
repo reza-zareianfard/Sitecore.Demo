d45c4498a1a403970634f149b0811011d2dbb36e
Dennis Lee <dle@codehousegroup.com>
2023-01-05 14:15:05 +1100

    removed unused imports

--- Website/src/rendering/src/components/Products/CategoryHero.tsx
+++ Website/src/rendering/src/components/Products/CategoryHero.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link';
 import { Category } from '../../models/Category';
 import CategoryBreadcrumb from '../../components/Navigation/CategoryBreadcrumb';
-import { getCategoryChildrenByCcid } from '../../helpers/CategoriesDataHelper';
 
 type CategoryHeroProps = {
   category: Category;
