c86d7ca726a61457579f0653059779241b707e3d
Dennis Lee <dle@codehousegroup.com>
2023-01-06 16:56:14 +1100

    revert summary page

--- Website/src/rendering/src/pages/shop/checkout/order-summary.tsx
+++ Website/src/rendering/src/pages/shop/checkout/order-summary.tsx
@@ -1,34 +1,13 @@
 import { ReactElement } from 'react';
 import Head from 'next/head';
 import { ShopLayout } from 'components/Products/Shop';
-import LineItemList from '../../../components/Checkout/LineItemList';
 import ThankYouSection from 'components/Checkout/ThankYouSection';
 import ThankYouHero from 'components/Checkout/ThankYouHero';
-import useOcCurrentCart from '../../../hooks/useOcCurrentCart';
 
 const OrderSummary = (): JSX.Element => {
-  const { order } = useOcCurrentCart();
-
-  const cartContent = order != null && (
-    <div className="order-review-details shop-container">
-      <h3>Order review</h3>
-      <div className="grid-container">
-        <div className="panel line-items-panel">
-          <div className="panel-header">
-            <h2>Items</h2>
-          </div>
-          <div className="panel-body">
-            <LineItemList editable={false} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-
   return (
     <>
       <ThankYouHero />
-      <div>{cartContent}</div>
       <ThankYouSection />
     </>
   );
